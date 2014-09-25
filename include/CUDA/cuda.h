d9b9ad7ae6adcd2a1ea32cc13f9e4d251f0bd4ca
Mauro Calderara <mcalderara@iis.ee.ethz.ch>
2014-09-25 10:26:48 +0200

    Add one init() routine for all GPU libraries

--- include/CUDA/cuda.h
+++ include/CUDA/cuda.h
@@ -12,9 +12,39 @@
 #ifndef LINALG_CUDA_CUDA_H_
 #define LINALG_CUDA_CUDA_H_
 
+#ifdef HAVE_MAGMA
+#include <magma.h>
+#include <magma_lapack.h>
+#endif
+
 // Keep this in alphabetical order
 #include "cuda_checks.h"
 #include "cuda_cublas.h"
 #include "cuda_memory_allocation.h"
+
+namespace LinAlg {
+
+namespace GPU {
+
+/** \brief            A wrapper to initialize all GPU related handles
+ */
+inline void init() {
+
+  // Initialize all CUBLAS handles
+  LinAlg::CUDA::CUBLAS::init();
+
+  // Same for CUSPARSE:
+  //LinAlg::CUDA::CUSPARSE::init();
+
+#ifdef HAVE_MAGMA
+  // Initialize MAGMA
+  magma_init();
+#endif
+
+};
+
+} /* namespace LinAlg::CUDA */
+
+} /* namespace LinAlg */
 
 #endif /* LINALG_CUDA_CUDA_H_ */
