9f37ff0ce3f6d1e54b5bbd2dacce51d426e7ac6f
Mauro Calderara <mcalderara@iis.ee.ethz.ch>
2014-09-28 00:08:55 +0200

    Add Fills::lapack_rand and Fills::unity
    To start with, lapack_rand is probably ok. In good C++ spirit I should
    probably add a random filler that takes an std::function to fill the
    matrix some time later.

--- include/fills.h
+++ include/fills.h
@@ -13,15 +13,19 @@
 #define LINALG_FILLS_H_
 
 #include <algorithm>      // std::fill_n
+#include <random>         // std::mt19337, std::uniform_real_distribution
+#include <ctime>          // std::time
 
 #include "dense.h"
 #include "sparse.h"
+#include "LAPACK/lapack.h"
 
 namespace LinAlg {
 
 namespace Fills {
 
-/** \brief            Fills a matrix with a constant value (preserving sparcity)
+/** \brief            Fills a matrix with a constant value (preserving sparcity
+ *                    for sparse matrices)
  *
  *  \param[in,out]    matrix
  *                    Matrix to operate on.
@@ -35,31 +39,20 @@ namespace Fills {
 template <typename T>
 inline void val(Dense<T>& matrix, T value) {
 
-  auto leading_dimension = matrix._leading_dimension;
-  auto rows              = matrix._rows;
-  auto cols              = matrix._cols;
-  auto begin             = matrix._begin();
-
-  // For contiguous matrices we can do it in one go
-  if (leading_dimension == rows) {
-
-    std::fill_n(begin, rows * cols, value);
-
-  } else { // Fill each column separately
-
-    for (int column = 0; column < cols; ++column) {
-
-      std::fill_n(begin + (column * leading_dimension), rows, value);
-
-    }
-
-  }
+  LAPACK::xLASET(value, value, matrix);
 
 }
 /** \overload
  */
 template <typename T>
 inline void val(Sparse<T>& matrix, T value) {
+
+#ifndef LINALG_NO_CHECK
+  if (matrix.location() != Location::host) {
+    throw excUnimplemented("val(): operation not supported for matrices not in "
+                           "main memory");
+  }
+#endif
 
   auto begin      = matrix._values.get();
   auto n_nonzeros = matrix._n_nonzeros;
@@ -79,13 +72,7 @@ inline void val(Sparse<T>& matrix, T value) {
 template <typename T>
 inline void zero(Dense<T>& matrix) {
 
-  // In IEEE 754 floating point and on many systems also integers could be set
-  // to zero using memset. Depending on the platform this could go terribly
-  // wrong but iff you feel like you need this feel free to override the below
-  // pass through with something using memset. Remember that this is memory
-  // bound anyway.
-
-  val(matrix, T(0));
+  val(matrix, cast<T>(0));
 
 }
 /** \overload
@@ -93,7 +80,117 @@ inline void zero(Dense<T>& matrix) {
 template <typename T>
 inline void zero(Sparse<T>& matrix) {
 
-  val(matrix, T(0));
+  val(matrix, cast<T>(0));
+
+}
+
+/** \brief            Sets diagonal elements to value a, offdiagonal elements
+ *                    to value b
+ *
+ *  \param[in,out]    matrix
+ *                    Matrix to operate on.
+ *
+ *  \param[in]        a
+ *                    Value to set the diagonal elements to.
+ *
+ *  \param[in]        b
+ *                    Value to set the offdiagonal elements to.
+ */
+template <typename T>
+inline void val_diag_offdiag(Dense<T>& matrix, T a, T b) {
+
+  LAPACK::xLASET(a, b, matrix);
+
+};
+
+/** \brief            Sets a (square) matrix to unity
+ *
+ *  \param[in,out]    matrix
+ *                    Matrix to operate on.
+ *
+ */
+template <typename T>
+inline void unity(Dense<T>& matrix) {
+
+#ifdef LINALG_NO_CHECKS
+  if (matrix.rows() != matrix.cols()) {
+    throw excBadArgument("unity(): input matrix not square")
+  }
+#endif
+
+  LAPACK::xLASET(cast<T>(0.0), cast<T>(1.0), matrix);
+
+};
+
+/** \brief            Sets the elements of a matrix to random values using
+ *                    LAPACK's xLARNV function
+ *
+ *  \param[in,out]    matrix
+ *                    Matrix to operate on.
+ *
+ *  \param[in]        distribution
+ *                    1: uniform on (0, 1)
+ *                    2: uniform on (-1, 1)
+ *                    3: normal on (0, 1)
+ *                    4: uniform on abs(z) < 1 (unit disc) (C_t, Z_t only)
+ *                    5: uniform on abs(z) = 1 (unit circle) (C_t, Z_t only)
+ *
+ *  \param[in|out]    seed
+ *                    A array of 4 integers in [0, 4095] with seed[3] being 
+ *                    odd, serving as the seed for the random number 
+ *                    generator. On exit, seed is updated.
+ *
+ */
+template <typename T>
+inline void lapack_rand(Dense<T>& matrix, I_t distribution, I_t* seed) {
+
+#ifdef LINALG_NO_CHECKS
+  if (distribution < 1 || distribution > 5) {
+    throw excBadArgument("lapack_rand(matrix, distribution, seed), "
+                         "distribution: input value '%d' is invalid (must be "
+                         "1, 2, or 3 for this matrix type)");
+  } else if ((distribution == 4 || distribution == 5) && 
+             (matrix._is_complex() == false)             ) {
+    throw excBadArgument("lapack_rand(matrix, distribution, seed), "
+                         "distribution: input value '%d' is only supported for "
+                         "complex matrices", distribution);
+  }
+  for (int i = 0; i < 4; ++i) {
+    if (seed[i] < 0 || seed[i] > 4095) {
+      throw excBadArgument("lapack_rand(matrix, distribution, seed), seed: "
+                           "input value seed[%d]=%d is invalid", i, seed[i]);
+    }
+  }
+  if (seed[3] % 2 == 0) {
+    throw excBadArgument("lapack_rand(matrix, distribution, seed), seed: "
+                         "input value seed[3]=%d is invalid (must be uneven)", 
+                         seed[3]);
+  }
+#endif
+
+  LAPACK::xLARNV(distribution, seed, matrix);
+
+};
+
+/** \brief            Sets the elements of a matrix to uniform random values
+ *                    using on the 1D (S_t, D_t) or 2D (C_t, Z_t) unit ball 
+ *                    (|x| < 1) using LAPACK's xLARNV function
+ *
+ *  \param[in,out]    matrix
+ *                    Matrix to operate on.
+ */
+template <typename T>
+inline void lapack_urand(Dense<T>& matrix) {
+
+  std::mt19937 random_engine;
+  random_engine.seed(std::time(0));
+  std::uniform_int_distribution<I_t> random_integer(0, 4095);
+
+  I_t distribution = (matrix._is_complex()) ? 1 : 4;
+  I_t seed[] = { random_integer(random_engine), random_integer(random_engine), 
+                 random_integer(random_engine), 1 };
+
+  lapack_rand(matrix, distribution, &seed[0]);
 
 }
 
