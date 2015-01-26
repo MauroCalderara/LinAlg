f73f572b04c29cde516a50a230a782f8f1f4897f
Mauro Calderara <mcalderara@iis.ee.ethz.ch>
2015-01-26 11:45:55 +0100

    Support for more format conversions

--- include/utilities/format_convert.h
+++ include/utilities/format_convert.h
@@ -22,6 +22,9 @@
 #include "../sparse.h"
 #include "../fills.h"
 #include "checks.h"
+#include "../streams.h"
+#include "../BLAS/copy.h"
+
 
 namespace LinAlg {
 
@@ -51,14 +54,15 @@ inline I_t count_nonzeros(const Dense<T>& matrix, SubBlock sub_block) {
 
   check_format(Format::ColMajor, matrix, "count_nonzeros()");
 
-  if ((sub_block.first_row > matrix.rows() - 1) || 
-      (sub_block.last_row > matrix.rows())         ) {
+  if ((sub_block.first_row < 0) ||
+      (sub_block.last_row > matrix.rows())        ) {
     throw excBadArgument("count_nonzeros(): requested row range is not "
                          "contained in the source matrix.");
   } else if ((sub_block.first_col > matrix.cols() - 1) || 
              (sub_block.last_col > matrix.cols())         ) {
     throw excBadArgument("count_nonzeros(): requested col range is not "
-                         "contained in the source matrix."); }
+                         "contained in the source matrix.");
+  }
 #endif
 
   I_t  n_nonzeros        = 0;
@@ -67,13 +71,13 @@ inline I_t count_nonzeros(const Dense<T>& matrix, SubBlock sub_block) {
   auto zero              = cast<T>(0.0);
 
   for (auto col = sub_block.first_col; col < sub_block.last_col; ++col) {
-  
+
     for (auto row = sub_block.first_row; row < sub_block.last_row; ++row) {
-    
+
       if (data[col * leading_dimension + row] != zero) ++n_nonzeros;
-    
+
     }
-  
+
   }
 
   return n_nonzeros;
@@ -97,15 +101,14 @@ inline I_t count_nonzeros(const Sparse<T>& matrix, SubBlock sub_block) {
 
   check_format(Format::CSR, matrix, "count_nonzeros()");
 
-  if ((sub_block.first_row > matrix.rows() - 1) || 
-      (sub_block.last_row > matrix.rows())         ) {
+  if ((sub_block.first_row < 0) || (sub_block.last_row > matrix.rows())) {
     throw excBadArgument("count_nonzeros(): requested row range is not "
                          "contained in the source matrix.");
-  } else if ((sub_block.first_col > matrix.cols() - 1) || 
-             (sub_block.last_col > matrix.cols())         ) {
+  } else if (matrix._minimal_index != matrix._maximal_index    &&
+             ((sub_block.first_col < matrix._minimal_index) ||
+              (sub_block.last_col  > matrix._maximal_index)   )  ) {
     throw excBadArgument("count_nonzeros(): requested col range is not "
-                         "contained in the source matrix.");
-  }
+                         "contained in the source matrix."); }
 #endif
 
   I_t n_nonzeros = 0;
@@ -159,7 +162,7 @@ inline I_t count_nonzeros(const Sparse<T>& matrix, SubBlock sub_block) {
  *                    Reference matrix for the reallocation
  *
  *  \param[in]        reference_sub_block
- *                    Optional: reference sub matrix specification. Default:  
+ *                    Optional: reference sub matrix specification. Default: 
  *                    use the whole of reference
  *
  *  \param[in]        location
@@ -366,7 +369,7 @@ inline void sparse2dense_host(const Sparse<T>& source, SubBlock sub_block,
   }
   check_format(Format::CSR, source, "sparse2dense_host()");
 
-  if ((sub_block.first_row > source._size - 1) || 
+  if ((sub_block.first_row < 0) ||
       (sub_block.last_row > source._size)         ) {
     throw excBadArgument("sparse2dense_host(): requested row range is not "
                          "contained in the source matrix.");
@@ -454,7 +457,6 @@ inline void sparse2dense_host(const Sparse<T>& source, SubBlock sub_block,
 
 }
 
-
 /** \brief            Add a subblock of a sparse matrix to a dense matrix
  *
  *
@@ -485,6 +487,278 @@ inline void sparse2dense_host(const Sparse<T>& source, I_t first_row,
                     destination);
 
 }
+
+
+///////////////////////
+// complex <-> realimag
+
+// Declarations of CUDA code wrappers
+#ifdef HAVE_CUDA
+void gpu_complex2realimag(int, int, float2*, int, float*, int, float*, int);
+void gpu_complex2realimag(int, int, double2*, int, double*, int, double*, 
+                          int);
+
+void gpu_realimag2complex(int, int, float, float*, int, float*, int, float2, 
+                          float2*, int);
+void gpu_realimag2complex(int, int, double, double*, int, double*, int, 
+                          double2, double2*, int);
+
+void gpu_csr2dense(double*, int*, int*, int, int, double*, int);
+void gpu_csr2dense_async(double*, int*, int*, int, int, double*, int, 
+                         cudaStream_t);
+#endif
+
+/** \brief            Convert a complex matrix to a pair of real matrices
+ *
+ *  C -> R + i * I
+ * 
+ *  \param[in]        C
+ *                    Complex matrix
+ *
+ *  \param[in]        R
+ *                    Non-complex matrix
+ *
+ *  \param[in]        I
+ *                    Non-complex matrix
+ */
+template <typename T, typename U>
+inline void complex2realimag(const Dense<U>& C, Dense<T>& R, Dense<T>& I) {
+
+  PROFILING_FUNCTION_HEADER
+
+  if (R.is_empty()) R.reallocate_like(C);
+  if (I.is_empty()) I.reallocate_like(C);
+
+#ifndef LINALG_NO_CHECKS
+  if (! (((type<U>() == Type::C) && (type<T>() == Type::S)) ||
+         ((type<U>() == Type::Z) && (type<T>() == Type::D))   ) ) {
+    throw excBadArgument("complex2realimage(C, R, I): template expanded "
+                         "to invalid data type combination");
+  }
+  check_device(C, R, I, "complex2realimag(C, R, I)");
+  check_same_dimensions(C, R, "complex2realimag(C, R, I), C, R");
+  check_same_dimensions(C, I, "complex2realimag(C, R, I), C, I");
+  check_input_transposed(C, "complex2realimag(C, R, I), C");
+  check_output_transposed(R, "complex2realimag(C, R, I), R");
+  check_output_transposed(I, "complex2realimag(C, R, I), I");
+  check_device(C, R, "complex2realimag(C, R, I), C, R");
+  check_device(C, I, "complex2realimag(C, R, I), C, I");
+#endif
+
+  auto location    = C._location;
+  auto line_length = (C._format == Format::ColMajor) ? C._rows : C._cols;
+  auto lines       = (C._format == Format::ColMajor) ? C._cols : C._rows;
+  auto C_ptr       = C._begin();
+  auto ldc         = C._leading_dimension;
+  auto A_ptr       = R._begin();
+  auto lda         = R._leading_dimension;
+  auto B_ptr       = I._begin();
+  auto ldb         = I._leading_dimension;
+
+  if (location == Location::host) {
+
+    for (auto line = 0; line < lines; ++line) {
+
+      for (auto element = 0; element < line_length; ++element) {
+
+        A_ptr[lda * line + element] = real(C_ptr[ldc * line + element]);
+        B_ptr[ldb * line + element] = imag(C_ptr[ldc * line + element]);
+
+      }
+
+    }
+
+  }
+#ifdef HAVE_CUDA
+  else if (location == Location::GPU) {
+
+    gpu_complex2realimag(line_length, lines, C_ptr, ldc, A_ptr, lda, B_ptr, 
+                         ldb);
+
+  }
+#endif
+
+#ifndef LINALG_NO_CHECKS
+  else {
+
+    throw excUnimplemented("complex2realimag(C, R, I) not available on " 
+                           "requested engine");
+
+  }
+#endif
+
+}
+
+/** \brief            Convert a pair of real matrices to a complex matrix
+ *
+ *  C = alpha * (R + i * I) + beta * C
+ *
+ *  \param[in]        alpha
+ *                    OPTIONAL. Default: 1.0
+ *
+ *  \param[in]        R
+ *
+ *  \param[in]        I
+ *
+ *  \param[in]        beta
+ *                    OPTIONAL: Default: 0.0
+ * 
+ *  \param[in]        C
+ */
+template <typename T, typename U>
+inline void realimag2complex(T alpha, const Dense<T>& R, const Dense<T>& I,
+                             U beta, Dense<U>& C) {
+
+  PROFILING_FUNCTION_HEADER
+
+  if (alpha == cast<T>(0.0)) return;
+  if (C.is_empty()) C.reallocate_like(R);
+
+#ifndef LINALG_NO_CHECKS
+  if (! (((type<U>() == Type::C) && (type<T>() == Type::S)) ||
+         ((type<U>() == Type::Z) && (type<T>() == Type::D))   ) ) {
+    throw excBadArgument("realimag2complex(alpha, R, I, beta, C): template "
+                         "expanded to invalid data type combination");
+  }
+  check_device(R, I, C, "realimag2complex(alpha, R, I, beta, C)");
+  check_same_dimensions(R, I, "realimag2complex(alpha, R, I, beta, C), R, I");
+  check_same_dimensions(R, C, "realimag2complex(alpha, R, I, beta, C), R, C");
+  check_input_transposed(R, "realimag2complex(alpha, R, I, beta, C), R");
+  check_input_transposed(I, "realimag2complex(alpha, R, I, beta, C), I");
+  check_output_transposed(C, "realimag2complex(alpha, R, I, beta, C), C");
+  check_device(R, I, "realimag2complex(alpha, R, I, beta, C), R, I");
+  check_device(R, C, "realimag2complex(alpha, R, I, beta, C), R, C");
+#endif
+
+  auto location    = R._location;
+  auto line_length = (R._format == Format::ColMajor) ? R._rows : R._cols;
+  auto lines       = (R._format == Format::ColMajor) ? R._cols : R._rows;
+  auto A_ptr       = R._begin();
+  auto lda         = R._leading_dimension;
+  auto B_ptr       = I._begin();
+  auto ldb         = I._leading_dimension;
+  auto C_ptr       = C._begin();
+  auto ldc         = C._leading_dimension;
+
+  if (location == Location::host) {
+
+    U rivalue;
+    U cvalue;
+
+    for (auto line = 0; line < lines; ++line) {
+
+      for (auto element = 0; element < line_length; ++element) {
+
+        rivalue = cast<U>(A_ptr[lda * line + element],
+                          B_ptr[lda * line + element]);
+        cvalue  = C_ptr[ldc * line + element];
+
+        C_ptr[ldc * line + element] = cast<U>(alpha) * rivalue + beta * cvalue;
+
+      }
+
+    }
+
+  }
+#ifdef HAVE_CUDA
+  else if (location == Location::GPU) {
+
+    gpu_realimag2complex(line_length, lines, alpha, A_ptr, lda, B_ptr, ldb, 
+                         beta, C_ptr, ldc);
+
+  }
+#endif
+
+#ifndef LINALG_NO_CHECKS
+  else {
+
+    throw excUnimplemented("realimag2complex(alpha, R, I, beta, C) not "
+                           "available on requested engine");
+
+  }
+#endif
+
+}
+/** \overload
+ */
+template <typename T, typename U>
+inline void realimag2complex(const Dense<T>& R, const Dense<T>& I,
+                             Dense<U>& C) {
+  realimag2complex(cast<T>(1.0), R, I, cast<U>(0.0), C);
+}
+
+#ifdef HAVE_CUDA
+/** \brief            Routine to convert a CSR matrix to a dense matrix in 
+ *                    Format::ColMajor on a GPU
+ *
+ *  \param[in]        sparse
+ *
+ *  \param[out]       dense
+ *
+ *  \param[in]        zero_first
+ */
+//template <typename T>
+inline void csr2dense_gpu(const Sparse<D_t>& sparse, Dense<D_t>& dense,
+                          bool zero_first = false) {
+
+  PROFILING_FUNCTION_HEADER
+
+  // TODO:
+  // make some checks
+
+  if (zero_first) LinAlg::Fills::zero(dense);
+
+  auto values_dev        = sparse._values.get();
+  auto indices_dev       = sparse._indices.get();
+  auto edges_dev         = sparse._edges.get();
+  auto rows              = sparse.rows();
+  auto first_index       = sparse._first_index;
+  auto dense_dev         = dense._begin();
+  auto leading_dimension = dense._leading_dimension;
+
+  gpu_csr2dense(values_dev, indices_dev, edges_dev, rows, first_index, 
+                dense_dev, leading_dimension);
+
+
+}
+
+/** \brief            Routine to asynchronously convert a CSR matrix to a 
+ *                    dense matrix in Format::ColMajor on a GPU
+ *
+ *  \param[in]        sparse
+ *
+ *  \param[out]       dense
+ *
+ *  \param[in]        zero_first
+ */
+//template <typename T>
+inline I_t csr2dense_gpu_async(const Sparse<D_t>& sparse, Dense<D_t>& dense,
+                                Stream& stream, bool zero_first = false) {
+
+  PROFILING_FUNCTION_HEADER
+
+  // TODO:
+  // make some checks
+
+  if (zero_first) LinAlg::Fills::zero(dense);
+
+  auto values_dev        = sparse._values.get();
+  auto indices_dev       = sparse._indices.get();
+  auto edges_dev         = sparse._edges.get();
+  auto rows              = sparse.rows();
+  auto first_index       = sparse._first_index;
+  auto dense_dev         = dense._begin();
+  auto leading_dimension = dense._leading_dimension;
+  auto cuda_stream       = stream.cuda_stream;
+
+  gpu_csr2dense_async(values_dev, indices_dev, edges_dev, rows, first_index, 
+                      dense_dev, leading_dimension, cuda_stream);
+
+
+  return 0;
+
+}
+#endif /* HAVE_CUDA */
 
 } /* namespace LinAlg::Utilities */
 
